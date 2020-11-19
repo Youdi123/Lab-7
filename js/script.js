7db5796b19010e6bcc15e37ca605ed8bedd69b00
Youdi123 <71049753+Youdi123@users.noreply.github.com>
2020-11-19 16:28:57 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function init() {
 //add your javascrip between these two lines of code
     var y = document.getElementsByTagName("h2").item(0).innerHTML;
     var s = document.getElementById("entryinput").value;
-    alert(y + ":" + s);
+    alert(y + ": " + s);
     document.getElementById("textoutput").innerHTML = s;
 }
 window.addEventListener('load', init);
