6ff282be21c2d0f92d3c423cf7b13fcf82e8c0bf
Youdi123 <71049753+Youdi123@users.noreply.github.com>
2020-11-18 23:01:21 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 function init(){
 //add your javascrip between these two lines of code
-  var m=document.querySelectorAll("h2.center").innerHTML;
+ 
   var s=document.getElementById("entryinput").vlaue;
-  alert(m+":"+s);
   document.getElementById("textoutput").innerHTML=s;
+  alert(m+":"+s);
 }
   window.addEventListener('load', init);
