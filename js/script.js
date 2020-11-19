8c352866819257422c40218805eec7eef828c132
Youdi123 <71049753+Youdi123@users.noreply.github.com>
2020-11-18 22:53:19 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function init(){
 //add your javascrip between these two lines of code
   var m=document.getElementsByTagName("h2").innerHTML;
   var s=document.getElementById("entryinput").vlaue;
-  document.getElementById("textoutput").innerHTML=s;
   alert(m+":"+s);
+  document.getElementById("textoutput").innerHTML=s;
 }
   window.addEventListener('load', init);
