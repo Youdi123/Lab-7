fa94e7e895bf2446a2524d3f7c528f02844ddae8
Youdi123 <71049753+Youdi123@users.noreply.github.com>
2020-11-19 18:35:48 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 function init() {
 //add your javascrip between these two lines of code
+    var m = document.getElementById("entrybutton");
     var s = document.getElementById("entryinput").value;
     var y = document.getElementsByTagName("h2").item(0).innerHTML;
     alert(y + ": " + s);
     document.getElementById("textoutput").innerHTML = s;
 }
+m.addEventListener("click", init);
 window.addEventListener('load', init);
