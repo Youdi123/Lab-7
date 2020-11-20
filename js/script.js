58512235106df35cbd4239e4285a392a11230db0
Youdi123 <71049753+Youdi123@users.noreply.github.com>
2020-11-19 18:44:02 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 function init() {
 //add your javascrip between these two lines of code
-    var s = document.getElementById("entryinput").value;
-    var y = document.getElementsByTagName("h2").item(0).innerHTML;
-    alert(y + ": " + s);
-    document.getElementById("textoutput").innerHTML = s;
+    var m = document.getElementById("entrybutton");
+    function v() {
+        var s = document.getElementById("entryinput").value;
+        var y = document.getElementsByTagName("h2").item(0).innerHTML;
+        alert(y + ": " + s);
+        document.getElementById("textoutput").innerHTML = s;
+    }
+    m.addEventListener("click", v);
 }
-var m = document.getElementById("entrybutton");
-m.addEventListener("click", init);
 window.addEventListener('load', init);
