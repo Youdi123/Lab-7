d63c1a5f2648b140d836e8f473e1d02d5b90cc80
Youdi123 <71049753+Youdi123@users.noreply.github.com>
2020-11-19 18:37:50 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 function init() {
 //add your javascrip between these two lines of code
-    var m = document.getElementById("entrybutton");
     var s = document.getElementById("entryinput").value;
     var y = document.getElementsByTagName("h2").item(0).innerHTML;
     alert(y + ": " + s);
     document.getElementById("textoutput").innerHTML = s;
 }
+var m = document.getElementById("entrybutton");
 m.addEventListener("click", init);
 window.addEventListener('load', init);
